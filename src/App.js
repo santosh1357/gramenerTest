49490e96753db10fea859b03a81c58bab1d09564
Prasad <sj6282@snaponglobal.com>
2018-12-06 16:09:44 +0530

    completed the requirements

--- src/App.js
+++ src/App.js
@@ -1,25 +1,85 @@
+/*jshint esversion: 6 */
+/*jshint ignore:start */
+
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      count: null,
+      boxes: [],
+    }
+  }
+ handleAdd(){
+    const count = this.state.count + 1 ;
+    let boxStyle;
+    if(count %2 === 0 && count%5 === 0){
+      boxStyle={backgroundColor: 'blue', borderTopColor: 'orange'};
+    } else if(count %2 !== 0 && count%5 === 0){
+      boxStyle={backgroundColor: 'black', borderTopColor: 'orange'};
+    } else if(count %2 === 0){
+       boxStyle={backgroundColor: 'blue'}
+    } else if(count %2 !== 0){
+       boxStyle={backgroundColor: 'black'}
+    }
+    const box = <div style= {boxStyle} className='box' key={count} id={count}>{count}</div>;
+    console.log(count)
+    this.setState({
+      count: count,
+      boxes: this.state.boxes.concat(box),
+    })
+  }
+
+  handleRemove(){
+    if(this.state.count < 1){
+      this.setState({
+        count: null
+      })
+      return;
+    }
+    const count = this.state.count === 1?null:this.state.count -1 ;
+    const boxes = this.state.boxes;
+    boxes.splice(-1);
+    this.setState({
+      count: count,
+      boxes: boxes
+    })  
+  }
+
   render() {
+    let fs = 12 + this.state.count + 'px';
+ 
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <button 
+            className="add"
+            onClick={() => this.handleAdd()}
+        >
+          Add Box
+        </button>
+        <button 
+            className="remove"
+            onClick={() => this.handleRemove()}
+        >
+          Remove Box
+        </button>
+        {this.state.count && (
+          <div className='body'>
+              <div className='boxes'>
+                {this.state.boxes}
+              </div>
+              <div className="totalCount">
+                <p style = {{fontSize: fs}} className="countMsg"> Total Box : {this.state.count}</p>
+              </div>
+          </div>      
+       )}
+        {!this.state.count && (
+          <div className="boxes">
+            <p  className='message'>No Boxes, Click on buttons to create boxes</p>
+          </div>
+        )}
       </div>
     );
   }
